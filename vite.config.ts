afb902a299d318e05a5b47e549325a89a832b3dd
Scott <scott@scott-hw.online>
2025-10-15 17:33:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/', // or '/your-repo-name/' if publishing at a sub-path
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './')
+    }
+  }
 })
