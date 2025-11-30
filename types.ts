b8b55770c4ffaf3b4b488f60bc8dd678ec843b24
Scott <scott@scott-hw.online>
2025-11-30 10:53:16 +0000

    Starting website again leaving the Ambient.mp3 and data.json

--- types.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export type SectionName = 'about' | 'projects' | 'contact' | 'mission-update' | 'policies'
-
-export interface Section {
-  id: SectionName;
-  title: string;
-  content: React.ReactNode;
-}
