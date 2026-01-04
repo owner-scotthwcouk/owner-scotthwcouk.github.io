88782d058364946128d4b4a455544e35ce6454dd
Scott <scott@scott-hw.online>
2026-01-04 11:56:18 +0000

    Create authConfig.js with MSAL configuration stub
    Added a temporary MSAL configuration stub for authentication.

---
+++ starship-portfolio/src/authConfig.js
@@ -0,0 +1,10 @@
+// Temporary stub to satisfy import in main.jsx
+// TODO: Configure proper MSAL authentication settings
+
+export const msalConfig = {
+  auth: {
+    clientId: "",
+    authority: "",
+    redirectUri: ""
+  }
+};
